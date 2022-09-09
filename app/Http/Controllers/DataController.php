d2d51a065c29e8c8c8e881380db95167f86e9f7b
w1lldone <wildan.arrahman@gmail.com>
2022-09-09 09:28:07 +0700

    Data endpoints

---
+++ app/Http/Controllers/DataController.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Data;
+use Illuminate\Http\Request;
+
+class DataController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Request $request)
+    {
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $this->authorize('create', Data::class);
+
+        $data = $request->validate([
+            'name' => 'required|string|max:100',
+            'description' => 'nullable|string|max:500'
+        ]);
+
+        $data = Data::create($data);
+
+        return $data;
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Data  $data
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Data $data)
+    {
+        $this->authorize('view', $data);
+
+        return $data;
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Data  $data
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Data $data)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Data  $data
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Data $data)
+    {
+        $this->authorize('update', $data);
+
+        $update = $request->validate([
+            'name' => 'string|max:100',
+            'description' => 'nullable|string|max:500',
+            'value' => 'nullable'
+        ]);
+
+        $data->update($update);
+
+        return $data;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Data  $data
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Data $data)
+    {
+        $this->authorize('delete', $data);
+
+        $data->delete();
+
+        return response()->noContent();
+    }
+}
