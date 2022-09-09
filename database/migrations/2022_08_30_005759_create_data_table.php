deb3f09b72fdbb472985c30d4bba7edd51ac65a0
w1lldone <wildan.arrahman@gmail.com>
2022-09-09 09:27:05 +0700

    Data and Dashboard Database models and migrations

--- database/migrations/2022_08_30_005759_create_data_table.php
+++ database/migrations/2022_08_30_005759_create_data_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateDataTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,11 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('data', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->boolean('is_superadmin')->nullable();
-            $table->rememberToken();
+            $table->string('description')->nullable();
+            $table->text('value')->nullable();
             $table->timestamps();
         });
     }
@@ -32,6 +29,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('data');
     }
 }
