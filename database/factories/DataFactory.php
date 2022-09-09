84651b7edc82e0d8850781cea1e47d60afbdbaf7
w1lldone <wildan.arrahman@gmail.com>
2022-09-09 09:27:42 +0700

    Factory and seeders

---
+++ database/factories/DataFactory.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class DataFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->word(),
+            'description' => $this->faker->sentence(),
+            'value' => $this->faker->numberBetween()
+        ];
+    }
+}
--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -20,6 +20,7 @@ class UserFactory extends Factory
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
+            'is_superadmin' => false,
         ];
     }
 
@@ -33,6 +34,16 @@ class UserFactory extends Factory
         return $this->state(function (array $attributes) {
             return [
                 'email_verified_at' => null,
+            ];
+        });
+    }
+
+    public function superadmin()
+    {
+        return $this->state(function (array $attributes)
+        {
+            return [
+                'is_superadmin' => true,
             ];
         });
     }
