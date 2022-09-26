6f4a12a302b6e2adf1d2a227ed4c8afc57964c13
Aldiansyah Triewicaksono <aldiansyahtrie@gmail.com>
2022-09-26 15:29:32 +0700

    BugFixes (#35)

--- resources/js/components/category/CategoryList.vue
+++ resources/js/components/category/CategoryList.vue
@@ -1,57 +1,71 @@
 <template>
     <div>
-        <table class="table table-bordered">
-            <thead class="table-light">
-                <tr>
-                    <th scope="col" class="text-center">Colour</th>
-                    <th scope="col">Name</th>
-                    <th scope="col">Description</th>
-                    <th scope="col">Created Date</th>
-                    <th scope="col">Action</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr v-for="(item, index) in data" :key="index">
-                    <td class="align-middle text-center">
-                        <i
-                            class="mdi mdi-square"
-                            :style="{
-                                color: item.colour,
-                                fontSize: '1.5rem',
-                            }"
-                        ></i>
-                    </td>
-                    <td class="align-middle">{{ item.name }}</td>
-                    <td class="align-middle">{{ item.description }}</td>
-                    <td class="align-middle">
-                        {{ formatDateTime(item.created_at) }}
-                    </td>
-                    <td class="align-middle">
-                        <button
-                            type="button"
-                            class="btn btn-outline-primary my-1"
-                            data-bs-toggle="modal"
-                            data-bs-target="#category-edit-modal"
-                            @click="editItem(item)"
-                        >
-                            Edit
-                        </button>
-                        <button
-                            type="button"
-                            class="btn btn-outline-danger my-1"
-                            data-bs-toggle="modal"
-                            data-bs-target="#category-delete-modal"
-                            @click="deleteItem(item)"
-                        >
-                            Delete
-                        </button>
-                    </td>
-                </tr>
-            </tbody>
-        </table>
-        <slot></slot>
-        <category-edit-modal :selectedData="selectedItem"></category-edit-modal>
-        <category-delete-modal :selectedData="selectedItem"></category-delete-modal>
+        <div class="table-responsive">
+            <table class="table table-bordered">
+                <thead class="table-light">
+                    <tr>
+                        <th scope="col" class="text-center" style="width: 80px;">Colour</th>
+                        <th scope="col" style="width: 185px;">Name</th>
+                        <th scope="col" style="width: 364px;">Description</th>
+                        <th scope="col" style="width: 219px;">Created Date</th>
+                        <th scope="col">Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr v-for="(item, index) in data" :key="index">
+                        <td class="align-middle text-center">
+                            <i
+                                class="mdi mdi-square"
+                                :style="{
+                                    color: item.colour,
+                                    fontSize: '1.5rem',
+                                }"
+                            ></i>
+                        </td>
+                        <td class="align-middle">{{ item.name }}</td>
+                        <td class="align-middle">{{ item.description }}</td>
+                        <td class="align-middle">
+                            {{ formatDateTime(item.created_at) }}
+                        </td>
+                        <td class="align-middle">
+                            <button
+                                type="button"
+                                class="btn btn-outline-primary my-1"
+                                data-bs-toggle="modal"
+                                data-bs-target="#category-edit-modal"
+                                @click="editItem(item)"
+                            >
+                                Edit
+                            </button>
+                            <button
+                                type="button"
+                                class="btn btn-outline-danger my-1"
+                                data-bs-toggle="modal"
+                                data-bs-target="#category-delete-modal"
+                                @click="deleteItem(item)"
+                            >
+                                Delete
+                            </button>
+                        </td>
+                    </tr>
+                    <!-- make table row if data is not found -->
+                    <tr v-if="data.length == 0">
+                        <td colspan="5" class="text-center">
+                            No category found
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+            <div class="d-flex justify-content-between">
+                <slot></slot>
+            </div>
+            <category-edit-modal
+                :selectedData="selectedItem"
+            ></category-edit-modal>
+            <category-delete-modal
+                :selectedData="selectedItem"
+            ></category-delete-modal>
+        </div>
     </div>
 </template>
 
