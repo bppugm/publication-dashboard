0daa632c48357b2328ad1003b640fc0a863a8afa
w1lldone <wildan.arrahman@gmail.com>
2022-09-28 14:00:16 +0700

    Validation for ounbound variables

--- resources/js/components/dashboard/DashboardShowWidgetForm.vue
+++ resources/js/components/dashboard/DashboardShowWidgetForm.vue
@@ -47,176 +47,189 @@
             ></button>
           </div>
           <div class="modal-body">
-            <form @submit.prevent="submit()" v-if="form.type">
-              <!-- RIBBON -->
-              <div class="form-group">
-                <label class="form-label"><b>Ribbon</b></label>
-                <div class="row row-cols-md-2 g-2">
-                  <div class="col-md-2">
-                    <input
-                      type="color"
-                      class="form-control form-control-color"
-                      v-model="form.ribbonColour"
-                    />
-                  </div>
-                  <div class="col-md-10">
-                    <input
-                      type="text"
-                      class="form-control"
-                      placeholder="Enter ribbon title"
-                      maxlength="12"
-                      v-model="form.ribbonText"
-                    />
-                    <div class="d-flex justify-content-between">
-                      <small class="form-text">Maximum 12 characters</small>
-                      <small class="form-text"
-                        >{{ form.ribbonText ? form.ribbonText.length : 0 }} of
-                        12</small
-                      >
+            <validation-observer v-slot="{ handleSubmit }">
+              <form @submit.prevent="handleSubmit(submit)" v-if="form.type">
+                <!-- RIBBON -->
+                <div class="form-group">
+                  <label class="form-label"><b>Ribbon</b></label>
+                  <div class="row row-cols-md-2 g-2">
+                    <div class="col-md-2">
+                      <input
+                        type="color"
+                        class="form-control form-control-color"
+                        v-model="form.ribbonColour"
+                      />
+                    </div>
+                    <div class="col-md-10">
+                      <input
+                        type="text"
+                        class="form-control"
+                        placeholder="Enter ribbon title"
+                        maxlength="12"
+                        v-model="form.ribbonText"
+                      />
+                      <div class="d-flex justify-content-between">
+                        <small class="form-text">Maximum 12 characters</small>
+                        <small class="form-text"
+                          >{{ form.ribbonText ? form.ribbonText.length : 0 }} of
+                          12</small
+                        >
+                      </div>
                     </div>
                   </div>
                 </div>
-              </div>
-              <!-- END RIBBON -->
+                <!-- END RIBBON -->
 
-              <!-- TITLE -->
-              <div class="form-group">
-                <label class="form-label"
-                  ><b>Title</b><sup class="text-danger">*</sup></label
-                >
-                <input
-                  type="text"
-                  class="form-control"
-                  v-model="form.title"
-                  maxlength="100"
-                  required
-                  placeholder="Enter widget title"
-                />
-                <div class="d-flex justify-content-between">
-                  <small class="form-text">Maximum 100 characters</small>
-                  <small class="form-text"
-                    >{{ form.title.length }} of 100</small
+                <!-- TITLE -->
+                <div class="form-group">
+                  <label class="form-label"
+                    ><b>Title</b><sup class="text-danger">*</sup></label
                   >
-                </div>
-              </div>
-              <!-- END TITLE -->
-
-              <!-- UNIT -->
-              <div class="form-group mt-3">
-                <label class="form-label"><b>Unit</b></label>
-                <input
-                  type="text"
-                  class="form-control"
-                  v-model="form.unit"
-                  maxlength="12"
-                  placeholder="Enter widget unit"
-                />
-                <div class="d-flex justify-content-between">
-                  <small class="form-text">Maximum 12 characters</small>
-                  <small class="form-text"
-                    >{{ form.unit ? form.unit.length : 0 }} of 12</small
-                  >
-                </div>
-              </div>
-              <!-- END UNIT -->
-
-              <!-- DESCRIPTION -->
-              <div class="form-group mt-3">
-                <label class="form-label"><b>Description</b></label>
-                <textarea
-                  v-model="form.description"
-                  class="form-control"
-                  cols="20"
-                  rows="5"
-                ></textarea>
-              </div>
-              <!-- END DESCRIPTION -->
-
-              <hr />
-
-              <!-- Values -->
-              <div class="form-group mt-3 mb-4">
-                <div class="d-flex justify-content-between align-items-center">
-                  <label class="form-label"><b>Values</b></label>
-                  <button
-                    type="button"
-                    class="btn btn-sm btn-outline-primary"
-                    @click="addValue()"
-                  >
-                    +
-                  </button>
-                </div>
-              </div>
-
-              <div
-                class="mt-2"
-                v-for="(value, index) in form.values"
-                :key="index"
-              >
-                <div class="row row-cols-md-3 g-2">
-                  <div class="col-md-3">
-                    <select
-                      class="form-control form-select"
-                      v-model="form.values[index].type"
+                  <input
+                    type="text"
+                    class="form-control"
+                    v-model="form.title"
+                    maxlength="100"
+                    required
+                    placeholder="Enter widget title"
+                  />
+                  <div class="d-flex justify-content-between">
+                    <small class="form-text">Maximum 100 characters</small>
+                    <small class="form-text"
+                      >{{ form.title.length }} of 100</small
                     >
-                      <option
-                        v-for="type in valueTypes"
-                        :value="type"
-                        :key="`${index}.${type}`"
-                      >
-                        {{ type }}
-                      </option>
-                    </select>
                   </div>
-                  <div class="col-md-8">
-                    <dashboard-show-widget-data-selector
-                      v-if="form.values[index].type == 'data'"
-                      v-model="form.values[index].text"
-                      @selected="onSelectedData"
-                    ></dashboard-show-widget-data-selector>
-                    <dashboard-show-widget-expression
-                      v-if="form.values[index].type == 'expression'"
-                      v-model="form.values[index].text"
-                      :variables="form.values[index].variables"
-                      @selected="onSelectedExpression(index, $event)"
-                    ></dashboard-show-widget-expression>
-                    <input
-                      v-if="form.values[index].type == 'text'"
-                      type="text"
-                      class="form-control"
-                      placeholder="Enter value"
-                      maxlength="12"
-                      required
-                      v-model="form.values[index].text"
-                    />
+                </div>
+                <!-- END TITLE -->
+
+                <!-- UNIT -->
+                <div class="form-group mt-3">
+                  <label class="form-label"><b>Unit</b></label>
+                  <input
+                    type="text"
+                    class="form-control"
+                    v-model="form.unit"
+                    maxlength="12"
+                    placeholder="Enter widget unit"
+                  />
+                  <div class="d-flex justify-content-between">
+                    <small class="form-text">Maximum 12 characters</small>
+                    <small class="form-text"
+                      >{{ form.unit ? form.unit.length : 0 }} of 12</small
+                    >
                   </div>
-                  <div class="col-md-1">
+                </div>
+                <!-- END UNIT -->
+
+                <!-- DESCRIPTION -->
+                <div class="form-group mt-3">
+                  <label class="form-label"><b>Description</b></label>
+                  <textarea
+                    v-model="form.description"
+                    class="form-control"
+                    cols="20"
+                    rows="5"
+                  ></textarea>
+                </div>
+                <!-- END DESCRIPTION -->
+
+                <hr />
+
+                <!-- Values -->
+                <div class="form-group mt-3 mb-4">
+                  <div
+                    class="d-flex justify-content-between align-items-center"
+                  >
+                    <label class="form-label"><b>Values</b></label>
                     <button
                       type="button"
-                      @click="removeValue(index)"
-                      class="btn btn-sm btn-outline-danger"
+                      class="btn btn-sm btn-outline-primary"
+                      @click="addValue()"
                     >
-                      <i class="mdi mdi-delete-outline"></i>
+                      +
                     </button>
                   </div>
                 </div>
-              </div>
 
-              <div class="row mt-3">
-                <div class="col-md-6 d-grid">
-                  <button
-                    type="button"
-                    class="btn btn-danger"
-                    @click="resetForm()"
-                  >
-                    Reset
-                  </button>
+                <div
+                  class="mt-2"
+                  v-for="(value, index) in form.values"
+                  :key="index"
+                >
+                  <div class="row row-cols-md-3 g-2">
+                    <div class="col-md-3">
+                      <select
+                        class="form-control form-select"
+                        v-model="form.values[index].type"
+                      >
+                        <option
+                          v-for="type in valueTypes"
+                          :value="type"
+                          :key="`${index}.${type}`"
+                        >
+                          {{ type }}
+                        </option>
+                      </select>
+                    </div>
+                    <div class="col-md-8">
+                      <dashboard-show-widget-data-selector
+                        v-if="form.values[index].type == 'data'"
+                        v-model="form.values[index].text"
+                        @selected="onSelectedData"
+                      ></dashboard-show-widget-data-selector>
+                      <dashboard-show-widget-expression
+                        v-if="form.values[index].type == 'expression'"
+                        v-model="form.values[index].text"
+                        :variables="form.values[index].variables"
+                        @selected="onSelectedExpression(index, $event)"
+                      ></dashboard-show-widget-expression>
+                      <input
+                        v-if="form.values[index].type == 'text'"
+                        type="text"
+                        class="form-control"
+                        placeholder="Enter value"
+                        maxlength="12"
+                        required
+                        v-model="form.values[index].text"
+                      />
+                      <validation-provider
+                        rules="nounbounds"
+                        v-slot="{ errors }"
+                      >
+                        <input type="hidden" v-model="form.values[index]" />
+                        <span class="text-danger">
+                          {{ errors[0] }}
+                        </span>
+                      </validation-provider>
+                    </div>
+                    <div class="col-md-1">
+                      <button
+                        type="button"
+                        @click="removeValue(index)"
+                        class="btn btn-sm btn-outline-danger"
+                      >
+                        <i class="mdi mdi-delete-outline"></i>
+                      </button>
+                    </div>
+                  </div>
                 </div>
-                <div class="col-md-6 d-grid">
-                  <button type="submit" class="btn btn-primary">Save</button>
+
+                <div class="row mt-3">
+                  <div class="col-md-6 d-grid">
+                    <button
+                      type="button"
+                      class="btn btn-danger"
+                      @click="resetForm()"
+                    >
+                      Reset
+                    </button>
+                  </div>
+                  <div class="col-md-6 d-grid">
+                    <button type="submit" class="btn btn-primary">Save</button>
+                  </div>
                 </div>
-              </div>
-            </form>
+              </form>
+            </validation-observer>
           </div>
         </div>
       </div>
@@ -226,9 +239,31 @@
 </template>
 
 <script>
-import DashboardShowWidgetExpression from "./DashboardShowWidgetExpression.vue";
+import { ValidationProvider, ValidationObserver, extend } from "vee-validate";
+import { Parser } from "expr-eval";
+
+extend("nounbounds", (value) => {
+    if (value.type != "expression") {
+        return true;
+    }
+
+    var parser = new Parser();
+
+    let subs = {};
+    value.variables.forEach((item) => {
+        subs[item.text] = item.data_id;
+    });
+    var vars = parser.parse(value.text).simplify(subs).variables();
+
+    if (vars.length == 0) {
+        return true;
+    }
+
+    return `Unbound variables found: ${vars.join(', ')}`;
+});
+
 export default {
-  components: { DashboardShowWidgetExpression },
+  components: { ValidationProvider, ValidationObserver },
   props: {
     widgets: Array,
     editedWidget: {
@@ -331,7 +366,6 @@ export default {
       this.onSelectedData(event.original);
     },
     submit() {
-
       this.$emit("submitted", {
         widget: this.form,
         data: this.selectedData,
