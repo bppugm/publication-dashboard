861e7a16f32e82df196ea2bbef9a8c33617cb3fe
ebifurei <aldiansyahtrie@gmail.com>
2022-10-14 14:52:10 +0700

    fix indent

--- resources/views/dashboard/show.blade.php
+++ resources/views/dashboard/show.blade.php
@@ -5,8 +5,8 @@
         <div class="row py-5">
             <div class="col-md-12">
                 <div class="d-flex justify-content-between">
-<h5 class="text-primary card-title mb-0">
-<b>DASHBOARD</b>
+                    <h5 class="text-primary card-title mb-0">
+                        <b>DASHBOARD</b>
                     </h5>
                     <nav aria-label="breadcrumb">
                         <ol class="breadcrumb">
